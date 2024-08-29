3702b4a817495b7c2f0b44f4ecdf9dcc2079b86b
Samuel Zamudio <szamudio15@hotmail.com>
2024-08-28 18:09:17 -0700

    sliding window v2

--- algorithms/sliding_window/largest_sum_of_size_n.py
+++ algorithms/sliding_window/largest_sum_of_size_n.py
@@ -1,4 +1,23 @@
-def largest_sum(list, n):
+def largest_sum_of_size_n(list, n):
+    if n == len(list):
+        return sum(list)
+
+    largest_sum = 0
+    left = 0
+    right = n - 1
+    for num in list:
+        if (right >= len(list)):
+            return largest_sum
+        count = left
+        curr_sum = 0
+        while count <= right:
+            curr_sum += list[count]
+            count += 1
+        largest_sum = max(largest_sum, curr_sum)
+        left = left + 1
+        right = right + 1
+
+def largest_sum_of_size_3(list, n=3):
 
     largest_sum = 0
     left = 0
@@ -18,5 +37,7 @@ def largest_sum(list, n):
 input_list = [4, 2, 1, 7, 8, 1, 2, 8, 1, 0]
 sub_array_size = 3
 
-answer = largest_sum(input_list, sub_array_size)
+answer = largest_sum_of_size_3(input_list, sub_array_size)
+print(answer)
+answer = largest_sum_of_size_n(input_list, 4)
 print(answer)
